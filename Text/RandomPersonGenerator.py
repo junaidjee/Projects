b85bf163ca17cfb71244089683b190eadf2aad0d
Kurtis Brown <kurtis@kurtisebear.com>
2014-02-04 13:42:04 +0000

    Remove files

--- Text/RandomPersonGenerator.py
+++
@@ -1,6 +0,0 @@
-from random import choice
-
-names = ["Dan Love", "Darrell", "liam", "Adam A", "John Thompson", "", "", "andre","becky","craig","anthony","jordan","adam S","Richard"]
-
-print choice(names)
-
